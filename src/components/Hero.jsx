31d84d8d7a974f6e3c7c7799d0742793e11ed039
HasanHafizurRahman <hasanshanto922@gmail.com>
2025-02-09 12:35:54 +0600

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,27 @@
-import ladentistaHero from "../assets/ladentistaHero.png"
+import ladentistaHero from "../assets/ladentistaHero.png";
+
 const Hero = () => {
   return (
-    <div className="px-[144px]">
+    <div className="px-[144px] pt-40">
       <div className="max-width mx-auto">
         <div className="flex items-center justify-between">
-          <div>
-<h1 className="text-[40px] font-bold">Enhance Your Dental Life</h1>
-<h1 className="text-[40px] text-primary-0 font-bold">Dental Profession</h1>
-<p>La Dentista, Your Digital Partner</p>
-          </div>
-          <div>
-            <img src={ladentistaHero} alt="ladentistaHero" />
-          </div>
+          {/* Left Section */}
+          <section>
+            <h1 className="text-[40px] font-bold">Enhance Your Dental Life</h1>
+            <h1 className="text-[40px] text-primary-0 font-bold">
+              Dental Profession
+            </h1>
+            <p>La Dentista, Your Digital Partner</p>
+          </section>
+
+          {/* Right Section */}
+          <section className="flex items-end">
+            <img src={ladentistaHero} alt="ladentistaHero" className="self-end" />
+          </section>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,35 @@ const Navbar = () => {
   return (
     <nav className="px-[144px] py-[10px] w-full">
       <div className="max-width mx-auto">
-        <Logo />
+        <div className="flex items-center justify-between">
+          <div>
+            <Logo />
+          </div>
+          <div>
+            <ul className="flex items-center gap-5">
+              <li>
+                <a href="#" className="font-medium">
+                  Company
+                </a>
+              </li>
+              <li>
+                <a href="#" className="font-medium">
+                  Expertise
+                </a>
+              </li>
+              <li>
+                <a href="#" className="font-medium">
+                  Responsibilty
+                </a>
+              </li>
+              <li>
+                <a href="#" className="font-medium">
+                  Products
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
   );
